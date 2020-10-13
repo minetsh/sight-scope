ca73cd8b7bfc35342aa464af59fecfe7edde9e1a
Felix <felix@xingren.com>
2020-10-13 17:24:10 +0800

    feat: 新增 payload 功能

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,31 @@ if (isWindow) {
   });
 }
 
+const uuid = () => {
+  return Number(Date.now() * 100 + Math.floor(Math.random() * 100)).toString(
+    36,
+  );
+};
+
 export const get = (key: string): any => {
   return scope[key];
+};
+
+export const putPayload = (data: any): string => {
+  const payloadId = uuid();
+  if (isWindow && isSessionStorage) {
+    sessionStorage.setItem(`__sight-scope_${payloadId}`, JSON.stringify(data));
+  }
+  return payloadId;
+};
+
+export const getPayload = (payloadId: string): any => {
+  if (isWindow && isSessionStorage) {
+    const data = sessionStorage.getItem(`__sight-scope_${payloadId}`);
+    if (data) {
+      return JSON.parse(data);
+    }
+  }
 };
 
 ss();
--- test/sight-scope.test.js
+++ test/sight-scope.test.js
@@ -1,5 +1,21 @@
-const { get } = require('..');
+const { get, putPayload, getPayload } = require('..');
 
 test('sight-scope', () => {
   expect(get('hello')).toBeUndefined();
+
+  const pid = putPayload('hi');
+  expect(getPayload(pid)).toBe('hi');
+
+  const pid2 = putPayload({
+    name: 'Felix',
+    extra: {
+      age: 20,
+    },
+  });
+  expect(getPayload(pid2)).toEqual({
+    name: 'Felix',
+    extra: {
+      age: 20,
+    },
+  });
 });
